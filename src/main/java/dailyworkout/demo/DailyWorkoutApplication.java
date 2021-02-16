8b84b018555b2ab76231ba88fe9ef4c46e48281e
Jack2ee-dev <jack2ee.dev@gmail.com>
2021-02-16 21:51:18 +0900

    initial commit

---
+++ src/main/java/dailyworkout/demo/DailyWorkoutApplication.java
@@ -0,0 +1,13 @@
+package dailyworkout.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyWorkoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyWorkoutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dailyworkout/demo/DailyWorkoutApplicationTests.java
@@ -0,0 +1,13 @@
+package dailyworkout.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyWorkoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
